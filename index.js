1da60fa135179de90a1e574db70f9296e97e73eb
omerspac <omerspac@gmail.com>
2024-03-31 16:37:04 +0500

    commited

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env
+import inquirer from "inquirer";
+let toDoList = [];
+let cond = true;
+while (cond) {
+    let addTask = await inquirer.prompt([
+        {
+            message: "What would you like to add in your To-Do List?",
+            name: "addedToDo",
+            type: "input",
+        },
+        {
+            message: "Would you like to add more?",
+            name: "addMore",
+            type: "list",
+            choices: ["Yes", "No"]
+        },
+    ]);
+    toDoList.push(addTask.addedToDo);
+    console.log(toDoList);
+    if (addTask.addMore == "No") {
+        cond = false;
+    }
+}
